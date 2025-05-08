1bcdcbe1ffc0f04c2410c07ab1037a118b1cf18e
Lucasfrrb <lucaszye380839@gmail.com>
2025-05-08 19:53:51 -0300

    Add files via upload

---
+++ weatheragend/aplicativo.py
@@ -0,0 +1,81 @@
+import requests
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+from io import BytesIO
+
+API_KEY = "SUA_CHAVE_DA_API"
+CIDADE = "Vitória,BR"
+URL = f"http://api.openweathermap.org/data/2.5/weather?q={CIDADE}&appid={API_KEY}&units=metric&lang=pt_br"
+
+'''Estrutura da Janela'''
+Tela_inicial = Tk()
+Tela_inicial.title("Water Diary")
+Tela_inicial.geometry("900x600")
+Tela_inicial.resizable(False, False)
+Tela_inicial.config(bg="Silver") 
+
+
+'''Titulo'''
+label_titulo = tk.Label(Tela_inicial, text="Previsão do Tempo", font=("Helvetica", 24, "bold"), bg="Silver")
+label_titulo.pack(pady=20)
+
+
+'''Icone do Tempo'''
+label_icone = tk.Label(Tela_inicial, bg="Silver")
+label_icone.pack()
+
+
+'''Tenoeratura'''
+label_temp = tk.Label(Tela_inicial, font=("Helvetica", 48), bg="Silver")
+label_temp.pack()
+
+
+'''descrição'''
+label_desc = tk.Label(Tela_inicial, font=("Helvetica", 20), bg="Silver")
+label_desc.pack()
+
+
+'''Umildade'''
+label_umidade = tk.Label(Tela_inicial, font=("Helvetica", 16), bg="Silver")
+label_umidade.pack()
+
+
+'''atualizar ao iniciar'''
+def atualizar_clima():
+    try:
+        resposta = requests.get(URL)
+        dados = resposta.json()
+        print(dados)
+
+        if resposta.status_code != 200:
+            raise Exception(dados.get("message", "Erro ao obter dados."))
+        
+        temp = dados['main']['temp']
+        descricao = dados['weather'][0]['description']
+        umidade = dados['main']['humidity']
+        icone_id = dados['weather'][0]['icon']
+
+        
+        label_temp.config(text=f"{temp:.1f} °C")
+        label_desc.config(text=descricao.capitalize())
+        label_umidade.config(text=f"Umidade: {umidade}%")
+
+        
+        icone_url = f"http://openweathermap.org/img/wn/{icone_id}@2x.png"
+        icone_img = Image.open(BytesIO(requests.get(icone_url).content))
+        icone_tk = ImageTk.PhotoImage(icone_img)
+        label_icone.config(image=icone_tk)
+        label_icone.image = icone_tk
+
+    except Exception as e:
+        print(f"Erro: {e}")  
+        messagebox.showerror("Erro", f"Não foi possível atualizar o clima.\n{e}")
+
+atualizar_clima()
+
+
+'''Inicia programa'''
+Tela_inicial.mainloop()
+
