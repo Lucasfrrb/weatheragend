7dd9f3620759ccbb8ae6397e56bc846e5a798336
Lucasfrrb <lucaszye380839@gmail.com>
2025-05-15 17:02:43 -0300

    Update aplicativo.py

--- weatheragend/aplicativo.py
+++ weatheragend/aplicativo.py
@@ -5,6 +5,68 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 from io import BytesIO
 import geocoder
+import os
+import sys
+from win32com.client import Dispatch
+import winshell
+from pathlib import Path
+import ctypes.wintypes
+import subprocess
+
+'''istalar o PyInstaller''' 
+def instalar_pyinstaller():
+    try:
+        import PyInstaller
+        print("PyInstaller já instalado.")
+    except ImportError:
+        print("Instalando PyInstaller...")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "pyinstaller"])
+
+def criar_exe(nome_script):
+    caminho_script = os.path.abspath(nome_script)
+    diretorio_script = os.path.dirname(caminho_script)
+    os.chdir(diretorio_script)
+    print(f"Diretório atual: {os.getcwd()}")
+    print(f"Compilando script: {caminho_script}")
+    comando = [sys.executable, "-m", "PyInstaller", "--onefile", "--windowed", caminho_script]
+    subprocess.check_call(comando)
+
+if __name__ == "__main__":
+    pasta_atual = os.path.dirname(os.path.abspath(__file__))
+    nome_do_script = os.path.join(pasta_atual, "previsaotempo.py")
+
+    if not os.path.isfile(nome_do_script):
+        print(f"Erro: arquivo {nome_do_script} não encontrado.")
+        sys.exit(1)
+
+    instalar_pyinstaller()
+    criar_exe(nome_do_script)
+
+
+
+'''Criar atalho na área de trabalho'''
+def criar_atalho_no_desktop(nome_atalho, caminho_alvo):
+    desktop = winshell.desktop()
+    caminho_atalho = os.path.join(desktop, f"{nome_atalho}.lnk")
+
+    shell = Dispatch('WScript.Shell')
+    atalho = shell.CreateShortcut(caminho_atalho)
+    atalho.TargetPath = caminho_alvo
+    atalho.WorkingDirectory = os.path.dirname(caminho_alvo)
+    atalho.IconLocation = caminho_alvo
+    atalho.save()
+    print(f"Atalho criado em: {caminho_atalho}")
+
+if __name__ == "__main__":
+    pasta_atual = os.path.dirname(os.path.abspath(__file__))
+    exe_path = os.path.join(pasta_atual, "dist", "previsaotempo.exe")
+
+    if not os.path.isfile(exe_path):
+        print("Erro: Executável não encontrado. Compile o exe antes de criar o atalho.")
+        sys.exit(1)
+
+    criar_atalho_no_desktop("PrevisaoTempo", exe_path)
+
 
 '''Localizar o usuario'''
 g = geocoder.ip('me')
@@ -67,7 +129,7 @@ def atualizar_clima():
         
         label_temp.config(text=f"{temp:.1f} °C")
         label_desc.config(text=descricao.capitalize())
-        label_umidade.config(text=f"Humildade: {umidade}%")
+        label_umidade.config(text=f"Umidade: {umidade}%")
 
         
         icone_url = f"http://openweathermap.org/img/wn/{icone_id}@2x.png"
